1a511b2e98c2439d9517f5b8c561e6bccf832dc1
indrajeet120 <babuyadavmajhawa@gmail.com>
2025-11-13 19:58:22 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react"
 import { Route ,Routes, Navigate} from 'react-router-dom'
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
-import Customize from './pages/customize';
+import Customize from './pages/Customize';
 import Customize2 from "./pages/Customize2";
 import { userDataContext } from "./context/UserContext";
 import Home from "./pages/Home";
