b4475c1ffd53a7ef85e3fdbcd86fd40aad0db030
indrajeet120 <babuyadavmajhawa@gmail.com>
2025-11-13 20:08:13 +0530

    Update auth.controllers.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -24,7 +24,7 @@ export const signUp = async(req,res)=>{
 
         const token= await genToken(user._id)
 
-        res.cookie("token",token,{httpOnly:true, maxAge:7*24*60*60*1000, sameSite:"strict",secure:false})
+        res.cookie("token",token,{httpOnly:true, maxAge:7*24*60*60*1000, sameSite:"None",secure:true})
 
 
         return res.status(201).json(user)
@@ -51,7 +51,7 @@ export const Login = async(req,res)=>{
 
         const token= await genToken(user._id)
 
-        res.cookie("token",token,{httpOnly:true, maxAge:7*24*60*60*1000, sameSite:"strict",secure:false})
+        res.cookie("token",token,{httpOnly:true, maxAge:7*24*60*60*1000, sameSite:"None",secure:true})
 
 
         return res.status(200).json(user)
