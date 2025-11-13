82075b3afdeb16ccf6f9aafcd28b04b1e3aca518
indrajeet120 <babuyadavmajhawa@gmail.com>
2025-11-13 19:48:05 +0530

    Update UserContext.jsx

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,66 +1,4 @@
-// import React, { createContext, useEffect, useState } from 'react'
 
-// export const userDataContext = createContext();
-// import axios from 'axios';
-
-
-
-// function UserContext({children}) {
-//     const serverUrl="http://localhost:8000"
-//     const  [userData, setUserData] = useState(null)
-//         const [frontendImage, setFrontendImage] = useState(null)
-//         const [backendImage, setBackendImage] = useState(null)
-//         const [selectedImage, setSelectedImage] = useState(null)
-
-
-//     const handleCurrentUser= async ()=>{
-//       try {
-        
-//         const result= await axios.get(`${serverUrl}/api/user/current`,{withCredentials:true})
-//         setUserData(result.data)
-//         console.log(result.data)
-
-//       } catch (error) {
-//         console.log(error)
-//       }
-//     }
-
-
-//     //feth the response from gemini
-//     const getGeminiResponse= async(command)=>{
-//       try {
-//         const result = await axios.post(`${serverUrl}/api/user/asktoassistant`,{command},{withCredentials:true})
-//         return result.data
-//       } catch (error) {
-//         console.log(error)
-        
-//       }
-//     }
-
-
-
-//     useEffect(()=>{
-//       handleCurrentUser()
-//     },[])
-
-//     const value={
-//          serverUrl,
-//          userData, setUserData,backendImage, setBackendImage
-//          ,frontendImage, setFrontendImage,selectedImage ,setSelectedImage,
-//          getGeminiResponse
-//     };
-//   return (
-    
-//         <userDataContext.Provider value={value}>
-//       {children}
-//         </userDataContext.Provider>
-     
-  
-//   );
-// }
-
-// export default UserContext;
-//export {userDataContext};
 
 
 
@@ -70,7 +8,7 @@ import axios from "axios";
 export const userDataContext = createContext();
 
 function UserContext({ children }) {
-  const serverUrl = "http://localhost:8000";
+  const serverUrl = "https://virtualassistantbakend.onrender.com";
   const [userData, setUserData] = useState(null);
 
   // 🔹 Extra states for images and selection
